5cae982e7a4668294ede4ee3b38af14dfb7e3b40
cmendez <carlosandresucr@gmail.com>
2021-05-28 21:51:33 -0600

    Ejemplos de semana 2, declarar, asignar, inicializar, scanner y concatenar

---
+++ src/semana2/Contatenar.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semana2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Andres
+ */
+public class Contatenar {
+    public static void main(String[] args){
+        
+        Scanner escaner = new Scanner(System.in);
+        
+        System.out.println("Universidad Latina de Costa Rica");
+        System.out.println("Facultad TICs");
+        System.out.println("Programación para TI");
+        System.out.println();
+        System.out.println("Por favor, digite un número: ");
+
+        int num2 = escaner.nextInt();
+        System.out.println("Por favor, digite otro número: ");
+        
+        int box1 = escaner.nextInt();
+
+        System.out.print("Resultado: ");
+        String nombre = "Carlos Méndez";
+        
+        //Ojo a esta forma, aqui declaramos una variable, pero se puede no usar
+        //String resultado = nombre + " " + num2 + " " + box1;
+        //System.out.println(resultado);
+        
+        System.out.println(nombre + " " + num2 + " " + box1);
+        
+        
+    }
+}
---
+++ src/semana2/MiSegundaClase.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semana2;
+
+/*Lab 2, en este programa se suman dos variables.
+coment 
+coment
+ */
+public class MiSegundaClase {
+
+    public static void main(String[] args) {
+
+        System.out.println("El resultado es: ");
+
+        int box1;
+        int num2;
+        int z;
+        box1 = 5;
+        num2 = 30;
+        z = box1 + num2;
+
+        //Esta linea imprime al usuario el resultado
+        System.out.println(z);
+
+        float salario;
+        salario = 1020239.43f;
+
+        double resultadoCientifico;
+        resultadoCientifico = 1234.12312342;
+
+        boolean siSalimosMediaNoche;
+        siSalimosMediaNoche = false;
+
+        String miNombre;
+        miNombre = "Carlos A. Méndez R.";
+
+        char tipoSangre;
+        tipoSangre = 'O';
+    }
+}
