4a9edd7a9ff171d330c0b4061debed657e5b5438
cmendez <carlosandresucr@gmail.com>
2021-08-20 21:47:38 -0600

    Se sube clase 14 quiz 3

---
+++ src/semana14/Quiz3.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semana14;
+
+/**
+ *
+ * @author Andres
+ * Respuesta: 
+ */
+public class Quiz3 {
+    public static void main3(String[] args) {
+       Short i = 122, j = 122;
+       if (i == j) {
+            System.out.println("true");
+       } else {
+        System.out.println("false");
+      }
+    }
+    public static void main(String[] args) {
+       Short i = 1222, j = 1222;
+       if (i == j) {
+            System.out.println("true");
+       } else {
+        System.out.println("false");
+      }
+    }
+}
